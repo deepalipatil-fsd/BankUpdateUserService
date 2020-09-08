59c34a7f59f2c6e36848b4253fe73be80a7df6de
Deepali Patil <deepalipatil15@gmail.com>
2020-09-08 19:50:02 +0530

    feat - Update spring security to work with postman

--- src/main/java/com/javacloud/updateuser/controller/UserUpdateController.java
+++ src/main/java/com/javacloud/updateuser/controller/UserUpdateController.java
@@ -5,6 +5,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,8 @@ public class UserUpdateController {
 //            if(ObjectUtils.isEmpty(account))
 //                return null; // throw exception instead
             URI uri = URI.create("http://localhost:8081/bank/update");
-
+            if ( this.restTemplate.getInterceptors().size()<1)
+                    this.restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor("user", "password"));
             AccountHolder account = this.restTemplate.postForObject(uri, accountHolder, AccountHolder.class);
             return new ResponseEntity(account, HttpStatus.OK);
         } catch (Exception e) {
@@ -37,7 +39,7 @@ public class UserUpdateController {
     }
 
     public ResponseEntity<AccountHolder> reliable(AccountHolder accountHolder) {
-        return  new ResponseEntity(accountHolder, HttpStatus.OK);
+        return  new ResponseEntity(new AccountHolder(), HttpStatus.OK);
     }
 
 }
